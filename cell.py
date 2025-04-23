651dff31d2c93207a75660e63bb06390d0663187
Per-Arne Holtmon Akø <paako@dsts.info>
2025-04-23 14:19:40 +0200

    Add Cell class and drawing functionality

---
+++ cell.py
@@ -0,0 +1,43 @@
+from window import Window
+from point import Point
+from line import Line
+
+class Cell:
+    def __init__(self, 
+                 x1: int, y1: int, x2: int, y2: int, window: Window,
+                 wall_color: str = "black",
+                 has_left_wall: bool = True, 
+                 has_right_wall: bool = True,
+                 has_top_wall: bool = True,
+                 has_bottom_wall: bool = True
+                 ) -> None:
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = window
+        # Precompute the four corner points
+        self._top_left = Point(x1, y1)
+        self._top_right = Point(x2, y1)
+        self._bottom_left = Point(x1, y2)
+        self._bottom_right = Point(x2, y2)
+        # Pre-compute potential wall lines
+        self._left_wall = Line(self._top_left, self._bottom_left)
+        self._right_wall = Line(self._top_right, self._bottom_right)
+        self._top_wall = Line(self._top_left, self._top_right)
+        self._bottom_wall = Line(self._bottom_left, self._bottom_right)
+        self._wall_color = wall_color
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+
+    def draw(self) -> None:
+        if self.has_top_wall:
+            self._win.draw_line(self._top_wall, self._wall_color)
+        if self.has_left_wall:
+            self._win.draw_line(self._left_wall, self._wall_color)
+        if self.has_bottom_wall:
+            self._win.draw_line(self._bottom_wall, self._wall_color)
+        if self.has_right_wall:
+            self._win.draw_line(self._right_wall, self._wall_color)
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from window import Window
 from line import Line
 from point import Point
+from cell import Cell
 
 def main() -> None:
     window = Window(800, 600)
-    window.draw_line(
-        line=Line(Point(10, 10), Point(20, 20)), fill_color="red"
-    )
-    window.draw_line(
-        Line(Point(150, 250), Point(456, 765)), "red"
-    )
+    # window.draw_line(
+    #     line=Line(Point(50, 50), Point(90, 90)), fill_color="red"
+    # )
+    # window.draw_line(
+    #     Line(Point(150, 250), Point(456, 565)), "red"
+    # )
+    cell1 = Cell(10, 10, 40, 40, window, "black")
+    cell1.has_left_wall = False
+    cell1.draw()
+    cell2 = Cell(130, 240, 400, 570, window, "red")
+    cell2.has_bottom_wall = False
+    cell2.draw()
     window.wait_for_close()
 
 if __name__ == "__main__":
