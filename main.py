2b6253345b06fc0317e2ad0483c75fcd4881b07d
Per-Arne Holtmon Akø <paako@dsts.info>
2025-04-23 13:12:47 +0200

    Initial bare bones window creation

---
+++ main.py
@@ -0,0 +1,8 @@
+from window import Window
+
+def main() -> None:
+    window = Window(800, 600)
+    window.wait_for_close()
+
+if __name__ == "__main__":
+    main()
---
+++ window.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width: int, height: int) -> None:
+        self.__root = Tk()
+        self.__root.title("A simple maze solver")
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, width=width, height=height)
+        self.__canvas.pack()
+        self.__running = False
+        
+    def redraw(self) -> None:
+        self.__root.update_idletasks()
+        self.__root.update()
+        
+    def wait_for_close(self) -> None:
+        self.__running = True
+        while self.__running:
+            self.redraw()
+            
+    def close(self) -> None:
+        self.__running = False
