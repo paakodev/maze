2b1b0e32dbc03b3c679527cd21e0ebdd3132e5b2
Per-Arne Holtmon Akø <paako@dsts.info>
2025-04-23 13:24:07 +0200

    Add primitive line drawing functionality

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from window import Window
+from line import Line
+from point import Point
 
 def main() -> None:
     window = Window(800, 600)
+    window.draw_line(
+        line=Line(Point(10, 10), Point(20, 20)), fill_color="red"
+    )
+    window.draw_line(
+        Line(Point(150, 250), Point(456, 765)), "red"
+    )
     window.wait_for_close()
 
 if __name__ == "__main__":
---
+++ point.py
@@ -0,0 +1,6 @@
+
+# 0,0 is top-left of screen
+class Point:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
